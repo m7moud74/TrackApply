8a616b29a5f987eae9057c047d90f5291ca4b5ec
Mahmoud Mostafa <mhmmoud289@gmail.com>
2026-08-19 18:00:21 +0300

    Add try-catch to email backgroubd

--- Features/JobApplication/Commands/Update/UpdateApplicationHandler.cs
+++ Features/JobApplication/Commands/Update/UpdateApplicationHandler.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
-public class UpdateApplicationHandler(AppDbContext context,ICacheService cache,NotificationCahnnel cahnnel)
+public class UpdateApplicationHandler(AppDbContext context, ICacheService cache, NotificationCahnnel cahnnel)
 {
     public async Task<Result<bool>> Handle(int id, UpdateApplicationCommand request, CancellationToken cancellationToken)
     {
-        var application = await context.JobApplications.Include(u=>u.User)
+        var application = await context.JobApplications.Include(u => u.User)
             .FirstOrDefaultAsync(j => j.JobApplicationId == id, cancellationToken);
 
         if (application is null)
@@ -16,18 +17,22 @@ public class UpdateApplicationHandler(AppDbContext context,ICacheService cache,N
         {
             application.Position = request.Position;
         }
-
+        bool statuscahnged = false;
         if (!string.IsNullOrWhiteSpace(request.Status))
         {
             application.Status = Enum.Parse<ApplicationStatus>(request.Status, ignoreCase: true);
+            statuscahnged = true;
         }
 
         await context.SaveChangesAsync(cancellationToken);
         await cache.RemoveAsync("JobApplicationList", cancellationToken);
 
-        var @event = new ApplicationStatusChangedEvent(application.UserId,
-        application.User.Email, application.Status.ToString());
-        await cahnnel.PublishAsync(@event,cancellationToken);
+        if (statuscahnged)
+        {
+            var @event = new ApplicationStatusChangedEvent(application.UserId,
+            application.User.Email, application.Status.ToString());
+            await cahnnel.PublishAsync(@event, cancellationToken);
+        }
         return Result<bool>.Success(true);
     }
 }
--- Workers/EmailBackGroundService.cs
+++ Workers/EmailBackGroundService.cs
@@ -6,13 +6,20 @@ public class EmailBackGroundService(NotificationCahnnel cahnnel,ILogger<EmailBac
 
         await foreach (var @event in cahnnel.ReadAllAsync(stoppingToken))
         {
-            logger.LogInformation("Sending email to {Email}... Your application {Id} is now {Status}", 
-                @event.UserEmail, @event.JobApplicationId, @event.NewStatus);
-            
-            //محاكاة وهمية لعملية إرسال إيميل بتاخد ثانيتين
-            await Task.Delay(2000, stoppingToken); 
-            
-            logger.LogInformation("Email sent successfully to {Email}!", @event.UserEmail);
+            try
+            {
+                logger.LogInformation("Sending email to {Email}... Your application {Id} is now {Status}",
+                    @event.UserEmail, @event.JobApplicationId, @event.NewStatus);
+
+                //محاكاة وهمية لعملية إرسال إيميل بتاخد ثانيتين
+                await Task.Delay(2000, stoppingToken);
+
+                logger.LogInformation("Email sent successfully to {Email}!", @event.UserEmail);
+            }
+            catch(Exception ex)
+            {
+                 logger.LogError(ex, "Failed to process notification for {Email}", @event.UserEmail);
+            }
         }
     }
 }
